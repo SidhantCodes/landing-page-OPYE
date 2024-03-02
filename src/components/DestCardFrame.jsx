8a05243a326a1c4bc0a06d48befaa92a5fe7a165
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-03-02 23:43:06 +0530

    Added autoplay to both the carousels

--- src/components/DestCardFrame.jsx
+++ src/components/DestCardFrame.jsx
@@ -1,40 +1,47 @@
 "use client"
-import styles from "../styles/DestCardFrame.module.css"
-import { useCallback } from "react"
+import { useCallback, useEffect } from "react"
 import DestinationCards from "./DestinationCards"
 import Image from 'next/image';
 import useEmblaCarousel from 'embla-carousel-react'
 import { fwdArrow, bkdArrow } from "../../public";
 import { destination } from "@/assets/constants";
+import styles from "../styles/DestCardFrame.module.css"
 
 const DestCardFrame = () => {
   const [emblaRef, emblaApi] = useEmblaCarousel({loop: true})
-  const scrollPrev = useCallback(() => {    if (emblaApi) emblaApi.scrollPrev()  }, [emblaApi])
-  const scrollNext = useCallback(() => {    if (emblaApi) emblaApi.scrollNext()  }, [emblaApi])
+  const scrollPrev = useCallback(() => { if (emblaApi) emblaApi.scrollPrev() }, [emblaApi])
+  const scrollNext = useCallback(() => { if (emblaApi) emblaApi.scrollNext() }, [emblaApi])
+
+  useEffect(() => {
+    if (!emblaApi) return
+    const timer = setInterval(() => emblaApi.scrollNext(), 3000) // Change 3000 to the desired autoplay interval in milliseconds
+    return () => clearInterval(timer)
+  }, [emblaApi])
+
   return (
     <>
-    <div className={`${styles.destFrame}`}>
-      {destination.map((dest, index) => (
-        <DestinationCards key={index} imageUrl={dest.destImg} destinationName={dest.destName} locDescription={dest.destDesc} />
-      ))}
-    </div>
-    <div className={`${styles.embla} ${styles.caraousel}`} ref={emblaRef}>
-      <div className={styles.embla__container}>
+      <div className={`${styles.destFrame}`}>
         {destination.map((dest, index) => (
-          <div key={index} className={styles.embla__slide}>
-            <DestinationCards imageUrl={dest.destImg} destinationName={dest.destName} locDescription={dest.destDesc} />
-          </div>
+          <DestinationCards key={index} imageUrl={dest.destImg} destinationName={dest.destName} locDescription={dest.destDesc} />
         ))}
       </div>
-      <div className={`${styles.arrow}`}>
-        <button className={`embla__prev`} onClick={scrollPrev}>
-          <Image src={bkdArrow} height={20} width={20} />
-        </button>
-        <button className={`embla__next`} onClick={scrollNext}>
-        <Image src={fwdArrow} height={20} width={20} />
-        </button>
+      <div className={`${styles.embla} ${styles.caraousel}`} ref={emblaRef}>
+        <div className={styles.embla__container}>
+          {destination.map((dest, index) => (
+            <div key={index} className={styles.embla__slide}>
+              <DestinationCards imageUrl={dest.destImg} destinationName={dest.destName} locDescription={dest.destDesc} />
+            </div>
+          ))}
+        </div>
+        {/* <div className={`${styles.arrow}`}>
+          <button className={`embla__prev`} onClick={scrollPrev}>
+            <Image src={bkdArrow} height={20} width={20} />
+          </button>
+          <button className={`embla__next`} onClick={scrollNext}>
+            <Image src={fwdArrow} height={20} width={20} />
+          </button>
+        </div> */}
       </div>
-    </div>
     </>
   )
 }
--- src/components/TravellerExpCaraousel.jsx
+++ src/components/TravellerExpCaraousel.jsx
@@ -1,112 +1,51 @@
-// "use client"
-// import { useCallback } from 'react'
-// import TravellerExpCard from './TravellerExpCard'
-// import { pfp1, fwdArrow, bkdArrow } from '../../public'
-// import useEmblaCarousel from 'embla-carousel-react'
-// import Image from 'next/image'
-// import style from "../styles/TravellerExpCaraousel.module.css"
-
-// const TravellerExpCaraousel = () => {
-//   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
-//   const scrollPrev = useCallback(() => {    if (emblaApi) emblaApi.scrollPrev()  }, [emblaApi])
-//   const scrollNext = useCallback(() => {    if (emblaApi) emblaApi.scrollNext()  }, [emblaApi])
-//   return (
-//     <div className={`${style.travelCar}`}>
-//       <div className={`${style.embla}`} ref={emblaRef}>
-//       <div className={`${style.embla__container}`}>
-//         <div className={`${style.embla__slide}`}>
-//           <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-//         </div>
-//         <div className={`${style.embla__slide}`}>
-//           <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-//         </div>
-//         <div className={`${style.embla__slide}`}>
-//           <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-//         </div>
-//         <div className={`${style.embla__slide}`}>
-//           <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-//         </div>
-//         <div className={`${style.embla__slide}`}>
-//           <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-//         </div>
-//       </div>
-//       <div className={`${style.arrow}`}>
-//         <button className={`embla__prev`} onClick={scrollPrev}>
-//           <Image src={bkdArrow} height={20} width={20} />
-//         </button>
-//         <button className={`embla__next`} onClick={scrollNext}>
-//         <Image src={fwdArrow} height={20} width={20} />
-//         </button>
-//       </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default TravellerExpCaraousel
-
 "use client"
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import TravellerExpCard from "./TravellerExpCard";
+import { useCallback, useEffect } from 'react'
+import TravellerExpCard from './TravellerExpCard'
 import { pfp1, fwdArrow, bkdArrow } from '../../public'
+import useEmblaCarousel from 'embla-carousel-react'
+import Image from 'next/image'
+import style from "../styles/TravellerExpCaraousel.module.css"
 
 const TravellerExpCaraousel = () => {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    // dots: true,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-    responsive: [
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-    ]
-  };
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+  const scrollPrev = useCallback(() => { if (emblaApi) emblaApi.scrollPrev() }, [emblaApi])
+  const scrollNext = useCallback(() => { if (emblaApi) emblaApi.scrollNext() }, [emblaApi])
+
+  useEffect(() => {
+    if (!emblaApi) return
+    const timer = setInterval(() => emblaApi.scrollNext(), 2000) // Change 3000 to the desired autoplay interval in milliseconds
+    return () => clearInterval(timer)
+  }, [emblaApi])
+
   return (
-    <div className="slider-container overflow-hidden mx-2">
-      <Slider {...settings}>
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
+    <div className={`${style.travelCar}`}>
+      <div className={`${style.embla}`} ref={emblaRef}>
+        <div className={`${style.embla__container}`}>
+          <div className={`${style.embla__slide}`}>
+            <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
+          </div>
+          <div className={`${style.embla__slide}`}>
+            <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
+          </div>
+          <div className={`${style.embla__slide}`}>
+            <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
+          </div>
+          <div className={`${style.embla__slide}`}>
+            <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
+          </div>
+          <div className={`${style.embla__slide}`}>
+            <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
+          </div>
         </div>
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
+        <div className={`${style.arrow}`}>
+          <button className={`embla__prev`} onClick={scrollPrev}>
+            <Image src={bkdArrow} height={20} width={20} />
+          </button>
+          <button className={`embla__next`} onClick={scrollNext}>
+            <Image src={fwdArrow} height={20} width={20} />
+          </button>
         </div>
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-        </div>
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-        </div>
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-        </div >
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-        </div>
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-        </div>
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-        </div>
-        <div>
-          <TravellerExpCard rating={5} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." name="Sidhant M." desg="CEO at Amazon" img={pfp1} />
-        </div>
-      </Slider>
+      </div>
     </div>
   )
 }
